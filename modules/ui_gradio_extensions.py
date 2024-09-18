76819f18e8a692cfd47794e4b64b2a7172cba720
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2024-09-19 02:19:03 +0400

    Update ui_gradio_extensions.py

--- modules/ui_gradio_extensions.py
+++ modules/ui_gradio_extensions.py
@@ -39,8 +39,7 @@ def javascript_html():
     head += f'<script type="text/javascript" src="{edit_attention_js_path}"></script>\n'
     head += f'<script type="text/javascript" src="{viewer_js_path}"></script>\n'
     head += f'<script type="text/javascript" src="{image_viewer_js_path}"></script>\n'
-    civitai_js_path = webpath('md_lib/civitai_helper.js')
-    head += f'<script type="text/javascript" src="{civitai_js_path}"></script>\n'
+    head += f'<script type="text/javascript" src="extentions/md_lib/civitai_helper.js"></script>\n'
     head += f'<meta name="samples-path" content="{samples_path}">\n'
 
     if args_manager.args.theme:
