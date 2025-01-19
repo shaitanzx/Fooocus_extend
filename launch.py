d656c20423f22200cca89a7a1912ef85149bbf04
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-01-19 08:28:02 +0400

    upadate

--- launch.py
+++ launch.py
@@ -153,5 +153,5 @@ config.default_base_model_name, config.checkpoint_downloads = download_models(
 
 config.update_files()
 init_cache(config.model_filenames, config.paths_checkpoints, config.lora_filenames, config.paths_loras)
-
+os.mkdir('batch_images')
 from webui import *
