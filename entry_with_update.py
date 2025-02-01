c5cefb67bac7a67f97cd1a62717ffda2b6ee27c8
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-02-01 20:37:33 +0400

    fix

--- entry_with_update.py
+++ entry_with_update.py
@@ -37,7 +37,8 @@ try:
         repo.reset(local_branch.target, pygit2.GIT_RESET_HARD)
         print("Fast-forward merge")
         import fooocus_version
-        print ('\033[93mUpadate to Fooocus {fooocus_version.version}\033[0m')
+        YELLOW, RESET = '\033[93m', '\033[0m'
+        print(f'{YELLOW}Upadate to Fooocus {fooocus_version}{RESET}')
     elif merge_result & pygit2.GIT_MERGE_ANALYSIS_NORMAL:
         print("Update failed - Did you modify any file?")
 except Exception as e:
