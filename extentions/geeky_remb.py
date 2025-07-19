19544bde57d0e87bf9a6dad54e15e62fe27eb9e8
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-07-20 01:52:08 +0400

    fix

--- extentions/geeky_remb.py
+++ extentions/geeky_remb.py
@@ -753,9 +753,9 @@ def on_ui_tabs():
                 result = process_image(foreground_input, background_image, *args)
                 if image_format != "PNG":
                     result = result.convert("RGB")
-                with tempfile.NamedTemporaryFile(delete=False, suffix=f".{image_format.lower()}") as temp_file:
-                    result.save(temp_file.name, format=image_format, quality=95 if image_format == "JPEG" else None)
-                    return temp_file.name, None
+                _, output_folder, _ = generate_temp_filename(folder=modules.config.path_outputs,extension=image_format.lower())
+                result.save(output_folder, format=image_format, quality=95 if image_format == "JPEG" else None)
+                return output_folder, None
             elif input_type == "Video" and result_type == "Video":
                 output_video = process_video(foreground_video, background_video if background_mode == "video" else None, *args)
                 _, output_folder, _ = generate_temp_filename(folder=modules.config.path_outputs,extension='mp4')
@@ -769,11 +769,10 @@ def on_ui_tabs():
                     os.system(f'ffmpeg -i "{temp_output}" -c:v libx264 -crf {int(20 - (video_quality / 5))} "{output_folder}"')
                     os.remove(temp_output)
                     output_video=output_folder
-                    #output_video = temp_output
+
                 return None, output_video
             elif input_type == "Image" and result_type == "Video":
-                with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
-                    output_path = temp_file.name
+                _, output_path, _ = generate_temp_filename(folder=modules.config.path_outputs,extension='mp4')
                 frame = cv2.cvtColor(np.array(foreground_input), cv2.COLOR_RGB2BGR)
                 height, width = frame.shape[:2]
                 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -791,9 +790,9 @@ def on_ui_tabs():
                     result = process_image(pil_frame, background_image, *args)
                     if image_format != "PNG":
                         result = result.convert("RGB")
-                    with tempfile.NamedTemporaryFile(delete=False, suffix=f".{image_format.lower()}") as temp_file:
-                        result.save(temp_file.name, format=image_format, quality=95 if image_format == "JPEG" else None)
-                        return temp_file.name, None
+                    _, output_folder, _ = generate_temp_filename(folder=modules.config.path_outputs,extension=image_format.lower())
+                    result.save(output_folder, format=image_format, quality=95 if image_format == "JPEG" else None)
+                    return output_folder, None
                 return None, None
 
         input_type.change(update_input_type, inputs=[input_type], outputs=[foreground_input, foreground_video])
