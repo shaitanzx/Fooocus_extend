a9d71c72b18b0b98bed0af2bc60437990802da95
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-08-11 15:40:55 +0400

    fix

--- modules/default_pipeline.py
+++ modules/default_pipeline.py
@@ -385,12 +385,6 @@ def process_diffusion(positive_cond, negative_cond, steps, switch, width, height
 
     decoded_latent = None
 
-    print('=====================================')
-#    tiling="x_only"
-#    copy_unet=target_unet
-#    target_unet=copy.deepcopy(copy_unet)
-#    print(copy_unet)
-
     def make_circular_asymm(model, tileX: bool, tileY: bool):
         
         for layer in [layer for layer in model.modules() if isinstance(layer, torch.nn.Conv2d)]:
@@ -399,27 +393,14 @@ def process_diffusion(positive_cond, negative_cond, steps, switch, width, height
             layer.paddingX = (layer._reversed_padding_repeated_twice[0], layer._reversed_padding_repeated_twice[1], 0, 0)
             layer.paddingY = (0, 0, layer._reversed_padding_repeated_twice[2], layer._reversed_padding_repeated_twice[3])
             layer._conv_forward = __replacementConv2DConvForward.__get__(layer, Conv2d)
-            print('-----------------------prepere',)
         return model
     def __replacementConv2DConvForward(self, input: Tensor, weight: Tensor, bias: Optional[Tensor]):
-        print('------------replace')
         working = F.pad(input, self.paddingX, mode=self.padding_modeX)
         working = F.pad(working, self.paddingY, mode=self.padding_modeY)
         return F.conv2d(working, weight, bias, self.stride, _pair(0), self.dilation, self.groups)
     
-    make_circular_asymm(target_unet.model, tile_x, tile_y)
-    #if tiling == "enable":
-    #    make_circular_asymm(target_unet.model, True, True)
-    #elif tiling == "x_only":
-    #    make_circular_asymm(target_unet.model, True, False)
-    #elif tiling == "y_only":
-    #    make_circular_asymm(target_unet.model, False, True)
-    #else:
-    #    make_circular_asymm(target_unet.model, False, False)    
-
-    print('------------process')
-    print('------------refiner_method',refiner_swap_method)
-
+    if tile_x or tile_y:
+        make_circular_asymm(target_unet.model, tile_x, tile_y)
 
     if refiner_swap_method == 'joint':
         sampled_latent = core.ksampler(
@@ -559,7 +540,6 @@ def process_diffusion(positive_cond, negative_cond, steps, switch, width, height
     images = core.pytorch_to_numpy(decoded_latent)
     modules.patch.patch_settings[os.getpid()].eps_record = None
     if tile_x or tile_y:
-        print('-----------------restore')
         for layer in [l for l in target_unet.model.modules() if isinstance(l, torch.nn.Conv2d)]:
             layer._conv_forward = torch.nn.Conv2d._conv_forward.__get__(layer, Conv2d)
     return images
