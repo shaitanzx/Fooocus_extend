3a532d7736df96917230086afc7334ef100dab12
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-08-17 00:15:00 +0400

    fix

--- extentions/tile_roll.py
+++ extentions/tile_roll.py
@@ -7,7 +7,7 @@ def start():
     with gr.Column():
         with gr.Row():
             tile_load = gr.Image(label="Upload file of tile", type="numpy")
-            tile_copy = gr.Image(label="Upload file of tile", type="numpy",visible=False)
+            #tile_copy = gr.Image(label="Upload file of tile", type="numpy",visible=False)
     with gr.Column():
         with gr.Row():
             y_shift = gr.Slider(label="Y Shift",minimum=0,maximum=1.5,step=1,value=0,interactive=True,visible=False)
@@ -19,7 +19,7 @@ def start():
         height, width = image.shape[:2]
         x_shift = gr.update(minimum=-(width/2),maximum=width/2,value=0,visible=True)
         y_shift = gr.update(minimum=-(height/2),maximum=height/2,value=0,visible=True)
-        return image,x_shift,y_shift,gr.update(visible=True)
+        return x_shift,y_shift,gr.update(visible=True)
     def shifting_x(shift,image):
         image = np.roll(image, shift, axis=1)
         return image
@@ -35,7 +35,7 @@ def start():
 
 
 
-    tile_load.upload(copy_tile, inputs=tile_load, outputs=[tile_copy,x_shift,y_shift,save_tile],show_progress=False)
+    tile_load.upload(copy_tile, inputs=tile_load, outputs=[x_shift,y_shift,save_tile],show_progress=False)
     tile_load.change(clear_tile, inputs=tile_load, outputs=[x_shift,y_shift,save_tile],show_progress=False)
-    x_shift.release(shifting_x, inputs=[x_shift,tile_copy],outputs=tile_load,show_progress=False)
-    y_shift.release(shifting_y, inputs=[y_shift,tile_copy],outputs=tile_load,show_progress=False)
+    x_shift.release(shifting_x, inputs=[x_shift,tile_load],outputs=tile_load,show_progress=False)
+    y_shift.release(shifting_y, inputs=[y_shift,tile_load],outputs=tile_load,show_progress=False)
