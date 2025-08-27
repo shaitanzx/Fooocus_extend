f4520eee08c05c829bc7ca11331ddd5cde128ca2
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-08-27 14:11:17 +0400

    fix

--- extentions/inswapper/face_swap.py
+++ extentions/inswapper/face_swap.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np
 from typing import List, Union, Dict, Set, Tuple
 import modules.config
+import modules.util
 from extentions.inswapper.swapper import process
 import extentions.batch as batch
 temp_dir=modules.config.temp_path+os.path.sep
@@ -44,6 +45,31 @@ def perform_face_swap(images, inswapper_source_image, inswapper_source_image_ind
   else:
     return np.array(restored_img)
 
+def process_insw(inswap_source_image_indicies, inswap_target_image_indicies):
+    batch_path_face=f"{temp_dir}batch_insw_face"
+    batch_path_image=f"{temp_dir}batch_insw_image"
+    batch_temp=f"{temp_dir}batch_temp"
+    batch_files_face=sorted([name for name in os.listdir(batch_path_face) if os.path.isfile(os.path.join(batch_path_face, name))])
+    batch_files_image=sorted([name for name in os.listdir(batch_path_image) if os.path.isfile(os.path.join(batch_path_image, name))])
+    batch_all=len(batch_files_face) * (batch_files_image) 
+    passed=1
+    for f_name_face in batch_files_face:
+        for f_name_image in batch_files_image:
+          print (f"\033[91m[Inswapper QUEUE] {passed} / {batch_all}. Filename face: {f_name_face}. Filename image: {f_name_image} \033[0m")
+          gr.Info(f"inswapper Batch: start element generation {passed}/{batch_all}. Filename face: {f_name_face}. Filename image: {f_name_image}") 
+          img_face = Image.open(batch_path_face+os.path.sep+f_name_face)
+          img_image = Image.open(batch_path_image+os.path.sep+f_name_image)
+          yield gr.update(value=img_face,visible=True),gr.update(value=img_image,visible=True),gr.update(visible=False)
+          image_face=np.array(img_face)
+          image_image=np.array(img_image)
+          img_insw=Image.fromarray(perform_face_swap,inputs=[image_image, image_face, inswap_source_image_indicies, inswap_target_image_indicies])
+          #name, ext = os.path.splitext(f_name)
+          #filename =  batch_temp + os.path.sep + name +'_cf'+ext
+          _, _, filename = modules.util.generate_temp_filename(folder=temp_dir)
+          img_insw.save(filename)
+        passed+=1
+    return gr.update(value=None,visible=False),(value=None,visible=False),gr.update(visible=True)
+
 def inswapper_gui2():
     def zip_enable(enable,single_file):
         if enable:
@@ -67,6 +93,7 @@ def inswapper_gui2():
                 files_single_face = gr.Files(label="Drag (Select) 1 or more Source Face images",file_count="multiple",
                                             file_types=["image"],visible=True,interactive=True,height=260)
                 image_single_face=gr.Image(label="Source Face",visible=False,height=260,interactive=True,type="filepath")
+                preview_face=gr.Image(label="Input face preview",visible=False,height=260,interactive=False)
             with gr.Row():
                 enable_zip_face = gr.Checkbox(label="Upload ZIP-file", value=False)
         with gr.Column():
@@ -75,6 +102,7 @@ def inswapper_gui2():
                 files_single_image = gr.Files(label="Drag (Select) 1 or more Source images",file_count="multiple",
                                             file_types=["image"],visible=True,interactive=True,height=260)
                 image_single_image=gr.Image(label="Source Image",visible=False,height=260,interactive=True,type="filepath")
+                preview_image=gr.Image(label="Input image preview",visible=False,height=260,interactive=False)
             with gr.Row():
                 enable_zip_image = gr.Checkbox(label="Upload ZIP-file", value=False)
         with gr.Column():
@@ -102,16 +130,24 @@ def inswapper_gui2():
     files_single_image.upload(fn=single_image,inputs=files_single_image,outputs=[image_single_image,files_single_image],show_progress=False)
     
 
-    start.click(lambda: (gr.update(visible=True, interactive=False),gr.update(visible=False)),outputs=[start,file_out]) \
-              .then(fn=batch.clear_dirs,inputs=ext_dir) \
-              .then(fn=batch.unzip_file,inputs=[file_in,files_single,enable_zip,ext_dir]) \
-              .then(fn=process, inputs=[poKeepPnm, poThreshold, poTransPNG, poTransPNGEps,poTransPNGQuant],
-                        outputs=[preview,file_out],show_progress=False) \
+    inswap_start.click(lambda: (gr.update(visible=True, interactive=False),gr.update(visible=False)),outputs=[start,file_out]) \
+              .then(fn=batch.clear_dirs,inputs=ext_dir_face) \
+              .then(fn=batch.clear_dirs,inputs=ext_dir_image) \
+              .then(fn=batch.unzip_file,inputs=[file_in_face,files_single_face,enable_zip_face,ext_dir_face]) \
+              .then(fn=batch.unzip_file,inputs=[file_in_image,files_single_image,enable_zip_image,ext_dir_image]) \
+              .then(lambda: (gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False),gr.update(visible=False)),
+                        outputs=[file_in_face,files_single_face,image_single_face,file_in_image,files_single_image,image_single_image])
+              .then(fn=process_insw, inputs=[inswap_source_image_indicies, inswap_target_image_indicies],
+                        outputs=[preview_face,preview_image,file_out],show_progress=False) \
               .then(lambda: (gr.update(visible=True, interactive=True),gr.update(visible=False)),outputs=[file_out,preview],show_progress=False) \
-              .then(fn=batch.output_zip_one, outputs=file_out) \
-              .then(lambda: (gr.update(visible=True, interactive=True)),outputs=start)
+              .then(fn=batch.output_zip_image, outputs=[image_out,file_out]) \
+              .then(fn=zip_enable,inputs=[enable_zip_face,files_single_face],outputs=[file_in_face,files_single_face,image_single_face],show_progress=False) \
+              .then(fn=zip_enable,inputs=[enable_zip_image,files_single_image],outputs=[file_in_image,files_single_image,image_single_image],show_progress=False) \
+              .then(fn=single_image,inputs=files_single_face,outputs=[image_single_face,files_single_face],show_progress=False) \
+              .then(fn=single_image,inputs=files_single_image,outputs=[image_single_image,files_single_image],show_progress=False)
+              .then(lambda: (gr.update(visible=True, interactive=True)),outputs=inswap_start)
     
     
     
     
-    inswap_start.click(perform_face_swap,inputs=[inswap_original_image, inswap_source_image, inswap_source_image_indicies, inswap_target_image_indicies],outputs=inswap_output)
+    #inswap_start.click(perform_face_swap,inputs=[inswap_original_image, inswap_source_image, inswap_source_image_indicies, inswap_target_image_indicies],outputs=inswap_output)
