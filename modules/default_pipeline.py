19828dc86ada47b35e58b480e27c3cd8c5ac4a24
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-09-12 12:23:53 +0400

    fix

--- modules/default_pipeline.py
+++ modules/default_pipeline.py
@@ -345,7 +345,7 @@ def get_candidate_vae(steps, switch, denoise=1.0, refiner_swap_method='joint'):
 @torch.inference_mode()
 def process_diffusion(positive_cond, negative_cond, steps, switch, width, height, image_seed, callback, sampler_name, 
         scheduler_name, latent=None, denoise=1.0, tiled=False, cfg_scale=7.0, refiner_swap_method='joint', 
-        disable_preview=False,tile_x=False,tile_y=False,layer_list=['False']):
+        disable_preview=False,tile_x=False,tile_y=False,layer_diff=['False']):
 
     target_unet, target_vae, target_refiner_unet, target_refiner_vae, target_clip \
         = final_unet, final_vae, final_refiner_unet, final_refiner_vae, final_clip
