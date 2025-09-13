53982cdfbe160b2acc904676217e42dfcdb026dc
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-09-14 03:17:41 +0400

    fix

--- modules/default_pipeline.py
+++ modules/default_pipeline.py
@@ -422,7 +422,7 @@ def process_diffusion(positive_cond, negative_cond, steps, switch, width, height
         unet = target_unet.clone()
         vae = target_vae
         clip = target_clip
-        
+        print('qqqqqqqqqqqqqqqqqqqqqq',LayerMethod.FG_TO_BLEND)
         if method in [LayerMethod.FG_TO_BLEND, LayerMethod.FG_BLEND_TO_BG, LayerMethod.BG_TO_BLEND, LayerMethod.BG_BLEND_TO_FG]:
             print('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
             if fg_image is not None:
