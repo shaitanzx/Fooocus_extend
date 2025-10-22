735fbad00c51cfb9a60db05ccda2189f018cf3a7
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-10-22 16:07:20 +0400

    fix

--- webui.py
+++ webui.py
@@ -1615,7 +1615,7 @@ with shared.gradio_root:
                              adm_scaler_negative, adm_scaler_end, refiner_swap_method, adaptive_cfg, clip_skip,
                              base_model, refiner_model, refiner_switch, sampler_name, scheduler_name, vae_name,
                              seed_random, image_seed, inpaint_engine, inpaint_engine_state,
-                             inpaint_mode, rescale_cfg] + enhance_inpaint_mode_ctrls + [generate_button,
+                             inpaint_mode] + enhance_inpaint_mode_ctrls + [generate_button,
                              load_parameter_button] + freeu_ctrls + lora_ctrls
         print('asasas',load_data_outputs)
         if not args_manager.args.disable_preset_selection:
