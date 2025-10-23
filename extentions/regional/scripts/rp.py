d7c4edb079f0b4410ab1c37d7772826252ba36f5
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-10-23 21:21:30 +0400

    fix

--- extentions/regional/scripts/rp.py
+++ extentions/regional/scripts/rp.py
@@ -13,9 +13,9 @@ from PIL import Image
 #!from modules.script_callbacks import (on_ui_settings, CFGDenoisedParams, CFGDenoiserParams, on_cfg_denoised, on_cfg_denoiser)
 import json  # Presets.
 from json.decoder import JSONDecodeError
-from extentions.regional.attention import (TOKENS, hook_forwards, reset_pmasks, savepmasks)
-from extentions.regional.latent import (denoised_callback_s, denoiser_callback_s, lora_namer, setuploras, unloadlorafowards, forge_linear_forward)
-from extentions.regionalregions import (MAXCOLREG, IDIM, KEYBRK, KEYBASE, KEYCOMM, KEYPROMPT, ALLKEYS, ALLALLKEYS,
+from extentions.regional.scripts.attention import (TOKENS, hook_forwards, reset_pmasks, savepmasks)
+from extentions.regional.scripts.latent import (denoised_callback_s, denoiser_callback_s, lora_namer, setuploras, unloadlorafowards, forge_linear_forward)
+from extentions.regional.scripts.regions import (MAXCOLREG, IDIM, KEYBRK, KEYBASE, KEYCOMM, KEYPROMPT, ALLKEYS, ALLALLKEYS,
                              create_canvas, draw_region, change_color,#detect_mask, detect_polygons,  
                              draw_image, save_mask, load_mask, changecs,
                              floatdef, inpaintmaskdealer, makeimgtmp, matrixdealer)
