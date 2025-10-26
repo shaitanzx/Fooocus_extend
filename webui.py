18a001abb3a23590fba431ffe41132ab7790b2a5
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-10-26 18:09:05 +0400

    fix

--- webui.py
+++ webui.py
@@ -65,10 +65,7 @@ from extentions.obp.scripts import onebuttonprompt as ob_prompt
 from extentions import tile_roll
 from extentions.vector import vector
 import extentions.batch as batch
-import extentions.watermark as watermark
-
-from extentions.adetailer.scripts.!adetailer import AfterDetailerScript as adetailer
-adetailer=Script()
+import extentions.watermark as watermarkz
 
 choices_ar1=["Any", "1:1", "3:2", "4:3", "4:5", "16:9"]
 choices_ar2=["Any", "1:1", "2:3", "3:4", "5:4", "9:16"]
@@ -772,8 +769,6 @@ with shared.gradio_root:
             with gr.Row(elem_classes='extend_row'):
               with gr.Accordion('Extention', open=False):
                 with gr.Accordion('in generation', open=False,elem_classes="nested-accordion") as gen_acc:
-                        with gr.TabItem(label='ADetailer'):
-                            adetailer.ui(is_img2img=False)
                         with gr.TabItem(label='OneButtonPrompt') as obp_tab:
                             (enable_obp,prompt1,prompt2,prompt3,prompt4,prompt5,
                                 prompt1toprompt,prompt2toprompt,prompt3toprompt,prompt4toprompt,prompt5toprompt,
