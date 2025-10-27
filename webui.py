08e0424778f5ddb6f7a73deb2f836c550f25a74b
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-10-27 22:49:27 +0400

    fix

--- webui.py
+++ webui.py
@@ -774,7 +774,8 @@ with shared.gradio_root:
                 with gr.Accordion('in generation', open=False,elem_classes="nested-accordion") as gen_acc:
                         with gr.TabItem(label='ADetailer'):
                             ad_component, ad_info = adetailer.ui(is_img2img=False)
-                            print('aaaaaaaaaaaaaaaa',adet)
+                            print('aaaaaaaaaaaaaaaa',ad_component)
+                            print('aaaaaaaaaaaaaaaa',ad_info)
                         with gr.TabItem(label='OneButtonPrompt') as obp_tab:
                             (enable_obp,prompt1,prompt2,prompt3,prompt4,prompt5,
                                 prompt1toprompt,prompt2toprompt,prompt3toprompt,prompt4toprompt,prompt5toprompt,
