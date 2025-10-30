78fb82dd8ece8464ffdbcf2645749cf7ffc02ae2
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-10-30 13:20:58 +0400

    fix

--- extentions/adetailer/aaaaaa/ui.py
+++ extentions/adetailer/aaaaaa/ui.py
@@ -166,7 +166,7 @@ def adui(
                 infotext_fields.extend(infofields)
 
     # components: [bool, bool, dict, dict, ...]
-    components = [ad_enable, ad_skip_img2img]
+    components = [ad_enable]
     return components, infotext_fields
 
 
