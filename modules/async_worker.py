33bf74c44d5dc6f6527d05ba0a4ee53852139d2d
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-11-03 19:15:49 +0400

    fix

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -1768,7 +1768,7 @@ def worker():
 
 
                     if async_task.debugging_adetailer_masks_checkbox:
-                        async_task.yields.append(['preview', (current_progress, 'Loading ...', masks[0])])
+                        async_task.yields.append(['preview', (current_progress, 'Loading ...', pred.preview)])
                         yield_result(async_task, masks, current_progress, async_task.black_out_nsfw, False,
                                      async_task.disable_intermediate_results)
                         #!async_task.enhance_stats[index] += 1
