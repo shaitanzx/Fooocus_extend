96ca908856db377bfbb6b278182402a88ab6b461
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-11-03 13:47:37 +0400

    fix

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -1392,55 +1392,7 @@ def worker():
             finally:
                 done_steps_upscaling += steps
         return current_task_id, done_steps_inpainting, done_steps_upscaling, img, exception_result
-    def process_adetailer(all_steps, async_task, callback, controlnet_canny_path, controlnet_cpds_path, 
-                        controlnet_pose_path, controlnet_recolor_path, controlnet_scribble_path,
-                        controlnet_manga_path,
-                        current_progress, current_task_id, denoising_strength, inpaint_disable_initial_latent,
-                        inpaint_engine, inpaint_respective_field, inpaint_strength,
-                        prompt, negative_prompt, final_scheduler_name, goals, height, img, mask,
-                        preparation_steps, steps, switch, tiled, total_count, use_expansion, use_style,
-                        use_synthetic_refiner, width, show_intermediate_results=True, persist_image=True):
-        base_model_additional_loras = []
-        inpaint_head_model_path = None
-        inpaint_parameterized = inpaint_engine != 'None'  # inpaint_engine = None, improve detail
-        initial_latent = None
 
-        prompt = prepare_enhance_prompt(prompt, async_task.prompt)
-        negative_prompt = prepare_enhance_prompt(negative_prompt, async_task.negative_prompt)
-
-        progressbar(async_task, current_progress, 'Downloading inpainter ...')
-        inpaint_head_model_path, inpaint_patch_model_path = modules.config.downloading_inpaint_models(
-                inpaint_engine)
-        if inpaint_patch_model_path not in base_model_additional_loras:
-                base_model_additional_loras += [(inpaint_patch_model_path, 1.0)]
-        progressbar(async_task, current_progress, 'Preparing adetail prompts ...')
-        # positive and negative conditioning aren't available here anymore, process prompt again
-        tasks_enhance, use_expansion, loras, current_progress = process_prompt(
-            async_task, prompt, negative_prompt, base_model_additional_loras, 1, True,
-            use_expansion, use_style, use_synthetic_refiner, current_progress)
-        task_enhance = tasks_enhance[0]
-        # TODO could support vary, upscale and CN in the future
-        # if 'cn' in goals:
-        #     apply_control_nets(async_task, height, ip_adapter_face_path, ip_adapter_path, width)
-        if async_task.freeu_enabled:
-            apply_freeu(async_task)
-        patch_samplers(async_task)
-        denoising_strength, initial_latent, width, height, current_progress = apply_inpaint(
-                async_task, None, inpaint_head_model_path, img, mask,
-                inpaint_parameterized, inpaint_strength,
-                inpaint_respective_field, switch, inpaint_disable_initial_latent,
-                current_progress, True)
-        imgs, img_paths, current_progress = process_task(all_steps, async_task, callback, controlnet_canny_path,
-                                                         controlnet_cpds_path, controlnet_pose_path, controlnet_recolor_path, 
-                                                         controlnet_scribble_path, controlnet_manga_path, current_task_id, denoising_strength,
-                                                         final_scheduler_name, goals, initial_latent, steps, switch,
-                                                         task_enhance['c'], task_enhance['uc'], task_enhance, loras,
-                                                         tiled, use_expansion, width, height, current_progress,
-                                                         preparation_steps, total_count, show_intermediate_results,
-                                                         persist_image)
-
-        del task_enhance['c'], task_enhance['uc']  # Save memory
-        return current_progress, imgs[0], prompt, negative_prompt
     @torch.no_grad()
     @torch.inference_mode()
     def handler(async_task: AsyncTask):
@@ -1695,7 +1647,7 @@ def worker():
 
                     base_model_additional_loras = []
                     inpaint_head_model_path = None
-                    inpaint_parameterized = inpaint_engine != 'None'  # inpaint_engine = None, improve detail
+                    inpaint_parameterized = async_task.inpaint_engine != 'None'  # inpaint_engine = None, improve detail
                     initial_latent = None
                     i1 = min(j, len(ad_prompts) - 1)
                     i2 = min(j, len(ad_negatives) - 1)
