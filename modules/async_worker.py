eb5642bad254b4fc2eff0ccbce73cfe0e1a3d631
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-11-03 10:48:45 +0400

    fix

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -1599,7 +1599,8 @@ def worker():
                 print ('aaaaaa mask',type(masks))
                 print ('aaaaaa mask',masks)
                 #!shared.state.assign_current_image(pred.preview)
-                pred.preview('/content/Fooocus_extend/preview.png')
+                pred.preview.save_image('/content/Fooocus_extend/preview.png')
+                masks[0].save_image('/content/Fooocus_extend/mask.png')
                 self.save_image(
                     p,
                     pred.preview,
