12e4dbd844b5766de4424701efd7622fdaf8a712
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-11-11 14:51:24 +0400

    fix

--- extentions/adetailer/aaaaaa/ui.py
+++ extentions/adetailer/aaaaaa/ui.py
@@ -265,14 +265,14 @@ def one_ui_group(n: int, is_img2img: bool, webui_info: WebuiInfo):
         widget.change(fn=on_change, inputs=[state, widget], outputs=state, queue=False)
 
     all_inputs = [state, *w.tolist()]
-    target_button = webui_info.i2i_button if is_img2img else webui_info.t2i_button
+    #!target_button = webui_info.i2i_button if is_img2img else webui_info.t2i_button
     #!target_button.click(
     #!    fn=on_generate_click, inputs=all_inputs, outputs=state, queue=False
     #!)
 
-    infotext_fields = [(getattr(w, attr), name + suffix(n)) for attr, name in ALL_ARGS]
+    #!infotext_fields = [(getattr(w, attr), name + suffix(n)) for attr, name in ALL_ARGS]
 
-    return state, infotext_fields
+    return state #!, infotext_fields
 
 
 def detection(w: Widgets, n: int, is_img2img: bool):
