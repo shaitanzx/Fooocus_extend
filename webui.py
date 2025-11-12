488fe6bdee04ea112a5a7319bdea7179df504383
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-11-12 23:48:03 +0400

    fix

--- webui.py
+++ webui.py
@@ -1608,8 +1608,8 @@ with shared.gradio_root:
                 def update_ad_model_choices():
                     model_list = modules.config.yolo_filenames
                     choices = ["None"] + model_list
-                    return [gr.update(choices=choices) for _ in range(modules.config.default_adetail_tab)],
-                    [gr.update(choices=["Use same checkpoint"]+modules.config.model_filenames) for _ in range(modules.config.default_adetail_tab)],
+                    return ([gr.update(choices=choices) for _ in range(modules.config.default_adetail_tab)],
+                    [gr.update(choices=["Use same checkpoint"]+modules.config.model_filenames) for _ in range(modules.config.default_adetail_tab)])
 
                 def refresh_files_clicked():
                     modules.config.update_files()
@@ -1628,7 +1628,7 @@ with shared.gradio_root:
                     refresh_files_output += [preset_selection]
                 refresh_files.click(refresh_files_clicked, [], refresh_files_output + lora_ctrls,
                                     queue=False, show_progress=False) \
-                                    .then(update_ad_model_choices,inputs=[],outputs=ad_model_dropdowns+ad_ckpt_dropdowns)
+                                    .then(update_ad_model_choices,inputs=[],outputs=ad_model_dropdowns,ad_ckpt_dropdowns)
 
         state_is_generating = gr.State(False)
 
