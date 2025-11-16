89f94c18a18169f2b6bc308bf9d9b81f8638c219
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-11-17 00:55:56 +0400

    fix

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -790,18 +790,6 @@ def worker():
                       inpaint_mask, inpaint_parameterized, denoising_strength, inpaint_respective_field, switch,
                       inpaint_disable_initial_latent, current_progress, skip_apply_outpaint=False,
                       advance_progress=False):
-
-        print('zzzzzzzzzzzzzz')
-        print('initial_latent',initial_latent)
-        print('inpaint_head_model_path',inpaint_head_model_path)
-        print('inpaint_parameterized',inpaint_parameterized)
-        print('denoising_strength',denoising_strength)
-        print('inpaint_respective_field',inpaint_respective_field)
-        print('switch',switch)
-        print('inpaint_disable_initial_latent',inpaint_disable_initial_latent)
-        print('current_progress',current_progress)
-        print('skip_apply_outpaint',skip_apply_outpaint)
-        print('advance_progress',advance_progress)
         if not skip_apply_outpaint:
             inpaint_image, inpaint_mask = apply_outpaint(async_task, inpaint_image, inpaint_mask)
 
@@ -1373,65 +1361,6 @@ def worker():
         del task_enhance['c'], task_enhance['uc']  # Save memory
         return current_progress, imgs[0], prompt, negative_prompt
 
-
-    def process_adetailer(all_steps, async_task, callback, controlnet_canny_path, controlnet_cpds_path, 
-                        controlnet_pose_path, controlnet_recolor_path, controlnet_scribble_path,
-                        controlnet_manga_path,
-                        current_progress, current_task_id, denoising_strength, inpaint_disable_initial_latent,
-                        inpaint_engine, inpaint_respective_field, inpaint_strength,
-                        prompt, negative_prompt, final_scheduler_name, goals, height, img, mask,
-                        preparation_steps, steps, switch, tiled, total_count, use_expansion, use_style,
-                        use_synthetic_refiner, width, show_intermediate_results=True, persist_image=True):
-        base_model_additional_loras = []
-        inpaint_head_model_path = None
-        inpaint_parameterized = inpaint_engine != 'None'  # inpaint_engine = None, improve detail
-        initial_latent = None
-        prompt = prepare_enhance_prompt(prompt, async_task.prompt)
-        negative_prompt = prepare_enhance_prompt(negative_prompt, async_task.negative_prompt)
-
-
-        if 'inpaint' in goals and inpaint_parameterized:
-            progressbar(async_task, current_progress, 'Downloading inpainter ...')
-            inpaint_head_model_path, inpaint_patch_model_path = modules.config.downloading_inpaint_models(
-                inpaint_engine)
-            if inpaint_patch_model_path not in base_model_additional_loras:
-                base_model_additional_loras += [(inpaint_patch_model_path, 1.0)]
-        progressbar(async_task, current_progress, 'Preparing enhance prompts ...')
-        # positive and negative conditioning aren't available here anymore, process prompt again
-        tasks_enhance, use_expansion, loras, current_progress = process_prompt(
-            async_task, prompt, negative_prompt, base_model_additional_loras, 1, True, 
-            use_expansion, use_style, use_synthetic_refiner, current_progress, advance_progress=False)
-        task_enhance = tasks_enhance[0]
-        # TODO could support vary, upscale and CN in the future
-        # if 'cn' in goals:
-        #     apply_control_nets(async_task, height, ip_adapter_face_path, ip_adapter_path, width)
-        if async_task.freeu_enabled:
-            apply_freeu(async_task)
-        patch_samplers(async_task)
-        if 'inpaint' in goals:
-            
-            denoising_strength, initial_latent, width, height, current_progress = apply_inpaint(
-                async_task, None, inpaint_head_model_path, img, mask, inpaint_parameterized, inpaint_strength,
-                inpaint_respective_field, switch, inpaint_disable_initial_latent, current_progress, 
-                skip_apply_outpaint=False, advance_progress=True)
-    
-
-
-
-        imgs, img_paths, current_progress = process_task(all_steps, async_task, callback, controlnet_canny_path,
-                                                         controlnet_cpds_path, controlnet_pose_path, controlnet_recolor_path, 
-                                                         controlnet_scribble_path, controlnet_manga_path, current_task_id, denoising_strength,
-                                                         final_scheduler_name, goals, initial_latent, steps, switch,
-                                                         task_enhance['c'], task_enhance['uc'], task_enhance, loras,
-                                                         tiled, use_expansion, width, height, current_progress,
-                                                         preparation_steps, total_count, show_intermediate_results,
-                                                         persist_image)
-
-        del task_enhance['c'], task_enhance['uc']  # Save memory
-        return current_progress, imgs[0], prompt, negative_prompt
-
-
-
     def enhance_upscale(all_steps, async_task, base_progress, callback, controlnet_canny_path, controlnet_cpds_path, 
                         controlnet_pose_path, controlnet_recolor_path, controlnet_scribble_path, controlnet_manga_path,
                         current_task_id, denoising_strength, done_steps_inpainting, done_steps_upscaling, enhance_steps,
@@ -1801,9 +1730,7 @@ def worker():
         exception_result = None
         if 'adetail' in goals or async_task.adetailer_checkbox:
             from extentions.adetailer.aaaaaa.helper import disable_safe_unpickle
-            if async_task.refiner_model_name == 'None':
-                        use_synthetic_refiner = True
-                        async_task.refiner_switch = 0.8
+
             
             for index, img in enumerate(images_to_adetailer):
                 async_task.adetailer_stats[index] = 0
@@ -1834,6 +1761,7 @@ def worker():
                                 #!device=ultralytics_device,
                                 classes=args.ad_model_classes,
                                 )
+                            print('zzzzzzz',pred)
 
                     masks = adetailer.pred_preprocessing(pred, args)
                     if len(masks) == 0:
@@ -1867,8 +1795,8 @@ def worker():
                         async_task.cfg_scale  = args.ad_cfg_scale
                     async_task.inpaint_disable_initial_latent = args.ad_disable_latent
                     if async_task.debugging_adetailer_masks_checkbox or async_task.only_detect:
-                        async_task.yields.append(['preview', (current_progress, 'Loading ...', masks[0])])
-                        yield_result(async_task, np.array(masks[0]), current_progress, async_task.black_out_nsfw, False,
+                        async_task.yields.append(['preview', (current_progress, 'Loading ...', pred.preview)])
+                        yield_result(async_task, np.array(pred.preview), current_progress, async_task.black_out_nsfw, False,
                                      async_task.disable_intermediate_results)
                         async_task.adetailer_stats[index] += 1
 
@@ -1887,11 +1815,8 @@ def worker():
                             prompt = prompt.replace("[PROMPT]", async_task.prompt)
                             negative = negative.replace("[PROMPT]", async_task.negative_prompt+' ')
 
-
-                            
-
                             try:
-                                current_progress, img, aadetail_prompt_processed, adetail_negative_prompt_processed = process_adetailer(
+                                current_progress, img, aadetail_prompt_processed, adetail_negative_prompt_processed = process_enhance(
                                     all_steps, async_task, callback, controlnet_canny_path, controlnet_cpds_path, 
                                     controlnet_pose_path, controlnet_recolor_path, controlnet_scribble_path,controlnet_manga_path,
                                     current_progress, current_task_id, denoising_strength, async_task.inpaint_disable_initial_latent,
