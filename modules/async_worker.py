a534af353a09c7e6d68bed1f917773d6317b53e9
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-11-27 19:02:15 +0400

    fix

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -884,6 +884,7 @@ def worker():
         return inpaint_image, inpaint_mask
 
     def apply_upscale(async_task, uov_input_image, uov_method, switch, current_progress, advance_progress=False):
+        print('22222222222222222222')
         H, W, C = uov_input_image.shape
         if advance_progress:
             current_progress += 1
@@ -1608,9 +1609,11 @@ def worker():
                 current_progress)
 
         if 'upscale' in goals:
+            print('111111111111111111111')
             direct_return, async_task.uov_input_image, denoising_strength, initial_latent, tiled, width, height, current_progress = apply_upscale(
                 async_task, async_task.uov_input_image, async_task.uov_method, switch, current_progress,
                 advance_progress=True)
+            print('33333333333333333333333333333')
             if direct_return:
                 d = [('Final Upscale', 'upscale_fast', async_task.uov_model)]
                 if modules.config.default_black_out_nsfw or async_task.black_out_nsfw:
