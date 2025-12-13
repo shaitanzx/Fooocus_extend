105d54be249a48b1346aa207a8d40c943aa060d0
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-12-13 17:37:33 +0400

    fix

--- webui.py
+++ webui.py
@@ -403,6 +403,7 @@ with shared.gradio_root:
 
                     def skip_clicked(currentTask):
                         print('zzzzzzzzzzzzzzzzzzzzzzzz',currentTask.last_stop)
+                        print('zzzzzzzzzzzzzzzzzzzzzzzz',currentTask.prompt)
                         import ldm_patched.modules.model_management as model_management
                         currentTask.last_stop = 'skip'
                         if (currentTask.processing):
