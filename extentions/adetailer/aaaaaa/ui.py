5fa35fc51d6cfcbb7b4d2c66ab7ccf6bc11e1e9e
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-12-13 09:45:58 +0400

    fix

--- extentions/adetailer/aaaaaa/ui.py
+++ extentions/adetailer/aaaaaa/ui.py
@@ -423,7 +423,7 @@ def inpainting(w: Widgets, n: int, is_img2img: bool, webui_info: WebuiInfo):  # 
         return [
             False, inpaint_engine_version, 1.0, 0.618
         ]
-    inpaint_mode.change(inpaint_mode_change, inputs=[w.inpaint_mode, w.ad_inpaint_engine], outputs=[
+    w.inpaint_mode.change(inpaint_mode_change, inputs=[w.inpaint_mode, w.ad_inpaint_engine], outputs=[
                             w.ad_disable_latent, w.ad_inpaint_engine,
                             w.ad_use_denoising_strength, w.ad_resp_field
                         ], show_progress=False, queue=False)            
