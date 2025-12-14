a06a7cc21d9bac09ee8dbef8b302d60da128f56b
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2025-12-14 22:31:32 +0400

    fix

--- webui.py
+++ webui.py
@@ -193,14 +193,14 @@ def prompt_batch(p,batch_prompt,positive_batch,negative_batch):
             currentTask.negative_prompt= one_batch_args[1] + p.negative_prompt
         else:
             currentTask.negative_prompt=one_batch_args[1]
-        if len(p.prompt)>0:
+        if len(currentTask.prompt)>0:
             yield from generate_clicked(currentTask)
             print('===================================',currentTask.last_stop)
             if currentTask.last_stop == 'break':
                 print('User skipped')
                 break        
         temp_var=currentTask.results
-        if currentTask.seed_random:
+        if p.seed_random:
             p.seed=int (random.randint(constants.MIN_SEED, constants.MAX_SEED))
         passed+=1
     return 
