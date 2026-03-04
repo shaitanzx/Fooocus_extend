eee3b7aa473bdcdb7a30796f6e637c296eaaaf3c
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-03-05 03:02:06 +0400

    fix

--- javascript/zoom.js
+++ javascript/zoom.js
@@ -644,6 +644,6 @@ onUiLoaded(async() => {
     applyZoomAndPan("#inpaint_canvas");
     applyZoomAndPan("#inpaint_mask_canvas");
     applyZoomAndPan("#cleaner_canvas");
-    applyZoomAndPan("#cleaner__video_canvas");
+    applyZoomAndPan("#cleaner_video_canvas");
     
 });
