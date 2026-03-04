560d7ccb02b2082ada0302e88ddcc420c9a35a97
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-03-04 21:45:53 +0400

    fix

--- extentions/cleaner/clean_up_tab.py
+++ extentions/cleaner/clean_up_tab.py
@@ -14,7 +14,7 @@ def clean_object_init_img_with_mask(init_img_with_mask):
         return clean_object(init_img_with_mask['image'],init_img_with_mask['mask']), gr.update(visible=True), gr.update(visible=True)
     else:
         print('-------------------------')
-        return clean_object(init_img_with_mask['image'],init_img_with_mask['mask']), gr.update(visible=False), gr.update(visible=False)
+        return None, gr.update(visible=False), gr.update(visible=False)
 
 def clean_object(image,mask):
     
