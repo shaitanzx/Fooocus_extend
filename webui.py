e9a3f290c37eac330e592a4a2e302c75f17dad0a
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-03-05 03:13:33 +0400

    fix

--- webui.py
+++ webui.py
@@ -536,14 +536,18 @@ with shared.gradio_root:
                         with gr.Row():
                             input_type = gr.Radio(["Image", "Video"], label="Input Type", value="Image")
                         with gr.Row() as image_clean:
-                            init_img_with_mask = grh.Image(label='Image', source='upload', type='pil', tool='sketch', height=500, brush_color="#FFFFFF", elem_id='cleaner_canvas', show_label=False)
+                            with gr.Row():
+                                init_img_with_mask = grh.Image(label='Image', source='upload', type='pil', tool='sketch', height=500, brush_color="#FFFFFF", elem_id='cleaner_canvas', show_label=False)
+                            with gr.Row():
+                                clean_button = gr.Button("Clean Up", height=100)
                         with gr.Row(visible=False)as video_clean:
-                            with gr.Column():    
-                                clean_video = gr.Video(label="Video", source='upload',visible=True,interactive=True,height=500)
-                            with gr.Column():
-                                clean_frame = grh.Image(label='Image', source='upload', type='pil', tool='sketch', height=500, brush_color="#FFFFFF", elem_id='cleaner_video_canvas', show_label=False,interactive=True)
-                        with gr.Row():
-                            clean_button = gr.Button("Clean Up", height=100)
+                            with gr.Row():
+                                with gr.Column():    
+                                    clean_video = gr.Video(label="Video", source='upload',visible=True,interactive=True,height=500)
+                                with gr.Column():
+                                    clean_frame = grh.Image(label='Image', source='upload', type='pil', tool='sketch', height=500, brush_color="#FFFFFF", elem_id='cleaner_video_canvas', show_label=False,interactive=True)
+                            with gr.Row():
+                                clean_button_video = gr.Button("Clean Up", height=100)
                         with gr.Row():    
                             
                             result_gallery = gr.Gallery(label='Gallery', show_label=False, object_fit='contain', visible=False, height=768,
