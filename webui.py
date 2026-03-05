f614cd73f0a1bf8105c3cbc599f39acf39d3f997
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-03-05 04:46:40 +0400

    fix

--- webui.py
+++ webui.py
@@ -544,7 +544,7 @@ with shared.gradio_root:
                                 with gr.Row():   
                                     clean_video = gr.Video(label="Video", source='upload',visible=True,interactive=True,height=500)
                                 with gr.Row(): 
-                                    clean_frame = grh.Image(label='Image', source='upload', type='pil', tool='sketch', height=500, brush_color="#FFFFFF", elem_id='cleaner_video_canvas', show_label=False)
+                                    clean_frame = grh.Image(label='Image', source='upload', type='pil', tool='sketch', height=500, brush_color="#FFFFFF", elem_id='cleaner_video_canvas', show_label=False,interactive=True)
                                 with gr.Row():
                                     clean_button_video = gr.Button("Clean Up", height=100,visible=True)
                         with gr.Row():                                
@@ -571,7 +571,7 @@ with shared.gradio_root:
                                 return None
                         input_type.change(update_input_type, inputs=[input_type], outputs=[image_clean, video_clean])
                         clean_video.upload(fn=get_first_frame, inputs=clean_video, outputs=clean_frame) \
-                            .then(lambda: (gr.update(visible=True,interactive=True),gr.update(visible=True)),outputs=[clean_frame,clean_button_video])
+                            .then(lambda: (gr.update(visible=True),gr.update(visible=True)),outputs=[clean_frame,clean_button_video])
                         clean_button.click(lambda: (gr.update(interactive=False)),outputs=[clean_button]) \
                             .then(fn=cleaner.clean_object_init_img_with_mask,inputs=[init_img_with_mask],outputs=[result_gallery,result_gallery,send_to_cleaner_button]) \
                             .then(lambda: (gr.update(interactive=True)),outputs=[clean_button])
