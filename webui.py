b7ba3d8a11bf80edfd25a36a658cd9a0764c728a
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-03-07 16:29:25 +0400

    fix

--- webui.py
+++ webui.py
@@ -552,15 +552,14 @@ with shared.gradio_root:
                                 with gr.Row():
                                     send_to_cleaner_button = gr.Button("Send back To clean up", height=100,visible=False)
                         with gr.Tab(label='Video'):
-                            with gr.Column(): 
-                                with gr.Сolumn():
-                                    video_files = gr.Files(label="Drag (Select) 1 or more video files",file_count="multiple",
+                            with gr.Сolumn():
+                                video_files = gr.Files(label="Drag (Select) 1 or more video files",file_count="multiple",
                                             file_types=["video"],visible=True,interactive=True)  
-                                    first_video = gr.Video(label="Video", source='upload',visible=True,interactive=True)
-                                with gr.Row(): 
-                                    clean_frame = grh.Image(label='First Frame',visible=False, source='upload', type='pil', tool='sketch', height=500, brush_color="#FFFFFF", elem_id='cleaner_video_canvas', show_label=False,interactive=True)
-                                with gr.Row():
-                                    clean_button_video = gr.Button("Clean Up", height=100,visible=False)
+                                first_video = gr.Video(label="Video", source='upload',visible=True,interactive=True)
+                            with gr.Row(): 
+                                clean_frame = grh.Image(label='First Frame',visible=False, source='upload', type='pil', tool='sketch', height=500, brush_color="#FFFFFF", elem_id='cleaner_video_canvas', show_label=False,interactive=True)
+                            with gr.Row():
+                                clean_button_video = gr.Button("Clean Up", height=100,visible=False)
                         
                         #def update_input_type(choice):
                         #    return gr.update(visible=choice == "Image"), gr.update(visible=choice == "Video")
