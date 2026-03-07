d7c81a5a8c2f21acb28e4d1f301902980545f5a8
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-03-07 17:18:23 +0400

    fix

--- webui.py
+++ webui.py
@@ -556,7 +556,7 @@ with shared.gradio_root:
                                     video_files = gr.Files(label="Drag (Select) 1 or more video files",file_count="multiple",
                                             file_types=["video"],visible=True,interactive=True)
                                 with gr.Column():  
-                                    first_video = gr.Video(label="Video", source='upload',visible=True,interactive=True)
+                                    first_video = gr.Video(label="Video", source='upload',visible=False,interactive=True)
                             with gr.Row(): 
                                 clean_frame = grh.Image(label='First Frame',visible=False, source='upload', type='pil', tool='sketch', height=500, brush_color="#FFFFFF", elem_id='cleaner_video_canvas', show_label=False,interactive=True)
                             with gr.Row():
