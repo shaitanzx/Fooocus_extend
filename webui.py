315860ce150165a68a6b4edb98aa9dba65cf0bd6
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-03-15 17:05:41 +0400

    fix

--- webui.py
+++ webui.py
@@ -1614,8 +1614,8 @@ with shared.gradio_root:
                                                                  outputs=[inpaint_mask_image, inpaint_mask_generation_col],
                                                                  queue=False, show_progress=False)
 
-                        inpaint_mask_color.change(lambda x: (gr.update(brush_color=x), gr.update(brush_color=x)), inputs=inpaint_mask_color,
-                                                  outputs=[inpaint_input_image,init_img_with_mask],
+                        inpaint_mask_color.change(lambda x: (gr.update(brush_color=x), gr.update(brush_color=x), gr.update(brush_color=x)), inputs=inpaint_mask_color,
+                                                  outputs=[inpaint_input_image,init_img_with_mask,clean_frame],
                                                   queue=False, show_progress=False)
 
                     with gr.Tab(label='FreeU'):
