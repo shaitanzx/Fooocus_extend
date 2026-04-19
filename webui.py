4599fefd48c194f160f3325fecc070b752001fde
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-04-19 17:39:25 +0400

    fix

--- webui.py
+++ webui.py
@@ -1124,10 +1124,10 @@ with shared.gradio_root:
             text_mask.select(html_load,inputs=[url_display,text_mask_file],outputs=mask,queue=False, show_progress=False)
             enhance_tab.select(lambda: 'enhance', outputs=current_tab, queue=False, _js=down_js, show_progress=False)
             metadata_tab.select(lambda: 'metadata', outputs=current_tab, queue=False, _js=down_js, show_progress=False)
-            enhance_checkbox.change(lambda x: gr.update(visible=x), inputs=enhance_checkbox,
-                                        outputs=enhance_input_panel, queue=False, show_progress=False, _js=switch_js)
-            adetailer_checkbox.change(lambda x: gr.update(visible=x), inputs=adetailer_checkbox,
-                                        outputs=adetailer_input_panel, queue=False, show_progress=False, _js=switch_js)
+            enhance_checkbox.change(lambda x: (gr.update(visible=x), gr.update(value=False, interactive=not x)),
+                        inputs=enhance_checkbox,outputs=[enhance_input_panel, adetailer_checkbox],queue=False, show_progress=False, _js=switch_js)
+            adetailer_checkbox.change(lambda x: (gr.update(visible=x), gr.update(value=False, interactive=not x)),
+                        inputs=adetailer_checkbox,outputs=[adetailer_input_panel, enhance_checkbox],queue=False, show_progress=False, _js=switch_js)
 
         with gr.Column(scale=1, visible=modules.config.default_advanced_checkbox) as advanced_column:
             with gr.Tab(label='Settings'):
