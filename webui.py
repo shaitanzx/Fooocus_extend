cadfefcd9a1b28fd24b077b76b7b047d01b7b4c5
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-05-10 09:18:33 +0400

    fix

--- webui.py
+++ webui.py
@@ -69,6 +69,7 @@ import extentions.watermark as watermark
 
 import extentions.adetailer.scripts.adetailer as adetailer
 import extentions.cleaner.clean_up_tab as cleaner
+import extentions.lbw.lbw as lbw
 choices_ar1=["Any", "1:1", "3:2", "4:3", "4:5", "16:9"]
 choices_ar2=["Any", "1:1", "2:3", "3:4", "5:4", "9:16"]
 
@@ -352,7 +353,8 @@ with shared.gradio_root:
             with gr.Row():
                 with gr.Column(scale=17):
                     prompt = gr.Textbox(show_label=False, placeholder="Type prompt here or paste parameters.", elem_id='positive_prompt',
-                                        autofocus=True, lines=3)
+                                        autofocus=True, lines=3,
+                                        value='masterpiece, best quality, ultra-detailed, (photorealistic:1.2), 1girl, white hair, blue eyes, looking at viewer, <lora:SDXL_FILM_PHOTOGRAPHY_STYLE_V1:1.0:::::start=10:stop=20>')
 
                     default_prompt = modules.config.default_prompt
                     if isinstance(default_prompt, str) and default_prompt != '':
@@ -768,8 +770,13 @@ with shared.gradio_root:
             ip_tab.select(lambda: 'ip', outputs=current_tab, queue=False, _js=down_js, show_progress=False)
             describe_tab.select(lambda: 'desc', outputs=current_tab, queue=False, _js=down_js, show_progress=False)
             with gr.Row(elem_classes='extend_row'):
-              with gr.Accordion('Extention', open=False):
-                with gr.Accordion('in generation', open=False,elem_classes="nested-accordion") as gen_acc:
+              with gr.Accordion('Extention', open=True):
+                with gr.Accordion('in generation', open=True,elem_classes="nested-accordion") as gen_acc:
+
+
+                        with gr.TabItem(label='LBW'):
+                            lbw_loraratios,lbw_useblocks,xyzsetting,xtype,xmen,ytype,ymen,ztype,zmen,exmen,eymen,ecount,diffcol,thresh,revxy,elemental,elemsets,debug = lbw.ui()
+
 
                         with gr.TabItem(label='OneButtonPrompt') as obp_tab:
                             (enable_obp,prompt1,prompt2,prompt3,prompt4,prompt5,
@@ -1877,8 +1884,11 @@ with shared.gradio_root:
         ctrls += [tile_x,tile_y]
         ctrls += [poKeepPnm, poThreshold, poTransPNG, poTransPNGEps,poDoVector,poTransPNGQuant]
         ctrls += [transper]
-
         ctrls += [uov_model]
+
+
+        ctrls += [lbw_loraratios,lbw_useblocks,xyzsetting,xtype,xmen,ytype,ymen,ztype,zmen,exmen,eymen,ecount,diffcol,thresh,revxy,elemental,elemsets,debug]
+        
         def ob_translate(workprompt,translate_enabled, srcTrans, toTrans):
             if translate_enabled:
                   workprompt, _ = translate(workprompt, "", srcTrans, toTrans)
