4f875a414560b0af321ec2931976aebd9fadfba3
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-05-22 13:04:02 +0400

    fix

--- modules/default_pipeline.py
+++ modules/default_pipeline.py
@@ -406,7 +406,7 @@ def process_diffusion(positive_cond, negative_cond, steps, switch, width, height
     unet = target_unet.clone()
     lbw_config = target_unet.model_options.get('lbw_config', {})
     lbw_loaded_loras = target_unet.model_options.get('_lbw_loaded_loras', [])
-    _lbw_state = {"cached_model": None}
+    _lbw_cached_model = {"cached_model": None}
     # 📦 Состояние кэширования (объявляются ОДИН раз перед обёрткой)
     def calc_loras(step_idx):
         lora_list = []
