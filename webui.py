5f2c947436db18e1e89aa9f89e0b5d18fcac236c
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-05-28 00:17:14 +0400

    fix

--- webui.py
+++ webui.py
@@ -353,7 +353,7 @@ with shared.gradio_root:
                 with gr.Column(scale=17):
                     prompt = gr.Textbox(show_label=False, placeholder="Type prompt here or paste parameters.", elem_id='positive_prompt',
                                         autofocus=True, lines=3,
-                                        value='masterpiece, best quality, ultra-detailed, (photorealistic:1.2), 1girl, white hair, blue eyes, looking at viewer, <lora:SDXL_FILM_PHOTOGRAPHY_STYLE_V1:w=1:lbw=ALL:start=10:stop=15>')
+                                        value='masterpiece, best quality, ultra-detailed, (photorealistic:1.2), 1girl, white hair, blue eyes, looking at viewer, <lora:SDXL_FILM_PHOTOGRAPHY_STYLE_V1:te=1:unet=1:lbw=ALL:start=10:stop=15>')
 
 
                     default_prompt = modules.config.default_prompt
