e6bfd0d47b2cefaf0ea9f2f85f45f382e825544c
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-06-03 18:08:12 +0400

    fix

--- webui.py
+++ webui.py
@@ -773,6 +773,7 @@ with shared.gradio_root:
               with gr.Accordion('Extention', open=False):
                 with gr.Accordion('in generation', open=False,elem_classes="nested-accordion") as gen_acc:
                         with gr.TabItem(label='DynamicButtonHelp') as dlora_tab:
+                            toggle_btn = gr.Button(value="ENG / RUS")
                             def toggle_html(current_html):
                                 # Определяем, какой файл сейчас загружен (можно по наличию русских букв или просто переключать)
                                 en_path = Path(__file__).parent / "dlora.html"
@@ -781,29 +782,22 @@ with shared.gradio_root:
                                 # Простая эвристика: если в HTML есть слово "Dynamic" - значит английский
                                 if "Dynamic" in current_html and "Динамический" not in current_html:
                                 # Переключаем на русский
-                                    return ru_path.read_text(encoding="utf-8"), "Switch to English"
+                                    return ru_path.read_text(encoding="utf-8")
                                 else:
                                 # Переключаем на английский
-                                    return en_path.read_text(encoding="utf-8"), "Переключить на русский"
+                                    return en_path.read_text(encoding="utf-8")
                                 # Загружаем начальную версию
                             en_path = Path(__file__).parent / "dlora.html"
                             initial_html = en_path.read_text(encoding="utf-8")
     
                             html_display = gr.HTML(value=initial_html)
-                            toggle_btn = gr.Button(value="Переключить на русский")
+                            
                             toggle_btn.click(
                                 fn=toggle_html,
                                 inputs=html_display,
-                                outputs=[html_display, toggle_btn]
+                                outputs=[html_display]
                                 )
-                            
-                            
-                            
-                            
-                            
-                            html_dlora_path = Path(__file__).parent / "dlora.html"
-                            html_dlora = html_dlora_path.read_text(encoding="utf-8")
-                            gr.HTML(value=html_dlora)
+
 
 
                         with gr.TabItem(label='OneButtonPrompt') as obp_tab:
