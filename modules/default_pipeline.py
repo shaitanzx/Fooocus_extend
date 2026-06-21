473e9dbc54af83a9e76606be3a8eb3c58d172c4c
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-06-21 07:34:27 +0400

    fix

--- modules/default_pipeline.py
+++ modules/default_pipeline.py
@@ -479,7 +479,8 @@ def process_diffusion(p, positive_cond, negative_cond, steps, switch, width, hei
     original_patches = copy.deepcopy(target_unet.patches)
     original_model_options = copy.deepcopy(target_unet.model_options)
 
-
+    original_pcond = copy.deepcopy(positive_cond)
+    original_ncond = copy.deepcopy(negative_cond)
     instantid_data = None
     if p.enable_instant:
         gen_width = width if width else 1152
@@ -795,6 +796,10 @@ def process_diffusion(p, positive_cond, negative_cond, steps, switch, width, hei
     target_unet.patches = copy.deepcopy(original_patches)
     target_unet.model_options = copy.deepcopy(original_model_options)
     del original_patches, original_model_options
+
+    positive_cond = copy.deepcopy(original_pcond)
+    negative_cond = copy.deepcopy(original_ncond)
+    del original_pcond, original_ncond
     
     # Очищаем instantid_data из памяти
     if instantid_data is not None:
