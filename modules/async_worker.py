b0e67c1b646a9a94951656fa435e3673aa977e3f
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-06-27 13:29:52 +0400

    fix

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -1334,15 +1334,13 @@ def worker():
 
         return base_model_additional_loras, clip_vision_path, controlnet_canny_path, controlnet_cpds_path, controlnet_pose_path, controlnet_recolor_path, controlnet_scribble_path, controlnet_manga_path, inpaint_head_model_path, inpaint_image, inpaint_mask, ip_adapter_face_path, ip_adapter_path, ip_negative_path, skip_prompt_processing, use_synthetic_refiner
     def apply_instant_cn(async_task,controlnet_canny_path,controlnet_cpds_path):
-
-
-            goals.append('cn')
-            progressbar(async_task, 1, 'Downloading control models ...')
-            if len(async_task.cn_tasks[flags.cn_canny]) > 0:
-                controlnet_canny_path = modules.config.downloading_controlnet_canny()
-            if len(async_task.cn_tasks[flags.cn_cpds]) > 0:
-                controlnet_cpds_path = modules.config.downloading_controlnet_cpds()
-            modules.config.download_instantid()
+        goals.append('cn')
+        progressbar(async_task, 1, 'Downloading control models ...')
+        if len(async_task.cn_tasks[flags.cn_canny]) > 0:
+            controlnet_canny_path = modules.config.downloading_controlnet_canny()
+        if len(async_task.cn_tasks[flags.cn_cpds]) > 0:
+            controlnet_cpds_path = modules.config.downloading_controlnet_cpds()
+        modules.config.download_instantid()
 
         return controlnet_canny_path, controlnet_cpds_path
 
