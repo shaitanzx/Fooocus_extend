dab6ce2565a521aa8f9643bcfa28260748bd9aa1
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-07-21 09:01:11 +0400

    fix

--- extentions/FaceEnhancer.py
+++ extentions/FaceEnhancer.py
@@ -1282,29 +1282,28 @@ def process_without_face(face_model,upscale_model,face_detection_only_center,fac
     batch_files=sorted([name for name in os.listdir(batch_path) if os.path.isfile(os.path.join(batch_path, name))])
     batch_all=len(batch_files)
     passed=1
+    for f_name in batch_files:
+        print (f"\033[91m[FaceEnhancer QUEUE] {passed} / {batch_all}. Filename image: {f_name} \033[0m")
+        gr.Info(f"FaceEnhancer Batch: start element generation {passed}/{batch_all}. Filename image: {f_name}")
+        img = Image.open(batch_path+os.path.sep+f_name)
 
-        for f_name in batch_files:
-            print (f"\033[91m[FaceEnhancer QUEUE] {passed} / {batch_all}. Filename image: {f_name} \033[0m")
-            gr.Info(f"FaceEnhancer Batch: start element generation {passed}/{batch_all}. Filename image: {f_name}")
-            img = Image.open(batch_path+os.path.sep+f_name)
-
-            yield gr.update(value=img,visible=True),gr.update(visible=False)
-            image=np.array(img)
+        yield gr.update(value=img,visible=True),gr.update(visible=False)
+        image=np.array(img)
 
 
-            img_cf = Image.fromarray(upscale.inference(
-                image, face_model, upscale_model, upscale_scale, face_detection, 
-                face_detection_threshold, face_detection_only_center, 
-                enable_swap, False, "0", "0'
-                ))
-            name, _ = os.path.splitext(f_name)
+        img_cf = Image.fromarray(upscale.inference(
+            image, face_model, upscale_model, upscale_scale, face_detection, 
+            face_detection_threshold, face_detection_only_center, 
+            enable_swap, False, "0", "0'
+            ))
+        name, _ = os.path.splitext(f_name)
 
-            suf = ''
-            if with_model_name:
-                suf=f'_{face_model}_{upscale_model}'
-            filename =  batch_temp + os.path.sep + name + suf + '.png'
-            img_cf.save(filename)
-            passed+=1
+        suf = ''
+        if with_model_name:
+            suf=f'_{face_model}_{upscale_model}'
+        filename =  batch_temp + os.path.sep + name + suf + '.png'
+        img_cf.save(filename)
+        passed+=1
     return gr.update(value=None,visible=False),gr.update(visible=True)
 
 
