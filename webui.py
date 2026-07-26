5488466d59d5cff9d73c830a34d8a7effd356359
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-07-26 22:53:08 +0400

    fix

--- webui.py
+++ webui.py
@@ -2129,7 +2129,7 @@ with shared.gradio_root:
     """)
     gr.HTML("""
         <div style="text-align: center;">
-            <p>Original code by <a href="https://github.com/lllyasviel/Fooocus" target="_blank">lllyasviel and other contributors.</a> Forked and extended by <a href="https://github.com/shaitanzx">Shahmatist^RMDA</a></p>        
+            <p>Original code by <a href="https://github.com/lllyasviel/Fooocus" target="_blank">lllyasviel and other contributors.</a> Forked and extended by <a href="https://github.com/shaitanzx">Shahmatist^RMDA.</a> Logo by ‎⁨𝓐𝓵𝓮𝔁 ☭ 𝓜𝓮⁩</p>        
         </div>
         """)
 
