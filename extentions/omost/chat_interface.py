76538fecbb19dd21ffc746fe71957cdba6c9be12
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-08-18 13:38:32 +0400

    fix

--- extentions/omost/chat_interface.py
+++ extentions/omost/chat_interface.py
@@ -27,6 +27,9 @@ from gradio.helpers import create_examples as Examples  # noqa: N812
 from gradio.layouts import Accordion, Column, Group, Row
 from gradio.themes import ThemeClass as Theme
 from gradio.utils import SyncToAsyncIterator, async_iteration
+
+set_documentation_group("chatinterface")
+
 from functools import wraps
 
 def async_lambda(f: Callable) -> Callable:
