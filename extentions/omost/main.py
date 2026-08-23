c3b82f185fa3409c72e8d8469022b48fa35df564
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-08-23 20:39:15 +0400

    fix

--- extentions/omost/main.py
+++ extentions/omost/main.py
@@ -404,7 +404,7 @@ def gui():
                 render_button = gr.Button("Render the Image!", size='lg', variant="primary", visible=False)
             with gr.Row(visible=False) as prompt_button:
                 prompt_key = gr.Button("Convert to prompt!", size='lg', variant="primary")
-                prompt_agress = gr.Radio(choices=['normal','aggressive','short'], value='aggressive',interactive=True)
+                prompt_agress = gr.Radio(label='Method',choices=['normal','aggressive','short'], value='aggressive',interactive=True)
 
             examples = gr.Dataset(
                 samples=[
