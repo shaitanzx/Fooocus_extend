9edd486b5834f598a3f742a6764d4c806701345d
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-09-07 13:52:21 +0400

    fix

--- webui.py
+++ webui.py
@@ -499,11 +499,15 @@ with shared.gradio_root:
                                 inpaint_advanced_masking_checkbox = gr.Checkbox(label='Enable Advanced Masking Features', value=modules.config.default_inpaint_advanced_masking_checkbox)
                                 inpaint_mode = gr.Dropdown(choices=modules.flags.inpaint_options, value=modules.config.default_inpaint_method, label='Method')
                                 inpaint_additional_prompt = gr.Textbox(placeholder="Describe what you want to inpaint.", elem_id='inpaint_additional_prompt', label='Inpaint Additional Prompt', visible=False)
-                                outpaint_selections = gr.CheckboxGroup(choices=['Left', 'Right', 'Top', 'Bottom'], value=[], label='Outpaint Direction')
+                                outpaint_selections = gr.CheckboxGroup(choices=['Left', 'Right', 'Top', 'Bottom', 'Resolution'], value=[], label='Outpaint Direction')
                                 example_inpaint_prompts = gr.Dataset(samples=modules.config.example_inpaint_prompts,
                                                                      label='Additional Prompt Quick List',
                                                                      components=[inpaint_additional_prompt],
                                                                      visible=False)
+                                outpaint_width = gr.Slider(label="outpaint width",minimum=0,maximum=1920,step=1,value=0,interactive=True,visible=False)
+                                outpaint_heighth = gr.Slider(label="outpaint height",minimum=0,maximum=1920,step=1,value=0,interactive=True,visible=False)
+                                outpaint_selections.select(lambda: (gr.update(visible=True, interactive=True), gr.update(visible=True, interactive=True)),
+                                                            outputs=[outpaint_width, outpaint_heighth])
                                 gr.HTML('* Powered by Fooocus Inpaint Engine <a href="https://github.com/lllyasviel/Fooocus/discussions/414" target="_blank">\U0001F4D4 Documentation</a>')
                                 example_inpaint_prompts.click(lambda x: x[0], inputs=example_inpaint_prompts, outputs=inpaint_additional_prompt, show_progress=False, queue=False)
 
