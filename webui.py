25435873f69267f05ae74254b085c97f289aae12
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-09-08 20:29:26 +0400

    fix

--- webui.py
+++ webui.py
@@ -524,8 +524,8 @@ with shared.gradio_root:
                                     return gr.update(maximum=shift_max,value=shift_center)
                                 def outpaint_resolution_selector(value):
                                      if 'Resolution' in value:
-                                         return gr.update(visible=True,interactive=True)
-                                     return gr.update(visible=False,interactive=True)
+                                         return gr.update(visible=True)
+                                     return gr.update(visible=False)
                                 outpaint_width.release(outpaint_shifting_width, inputs=[outpaint_width,outpaint_width_min],outputs=outpaint_shift_width,show_progress=False)
                                 outpaint_heighth.release(outpaint_shifting_width, inputs=[outpaint_heighth,outpaint_heighth_min],outputs=outpaint_shift_heighth,show_progress=False)
                                 inpaint_input_image.upload(fn=outpaint_resolution_value,inputs=inpaint_input_image,outputs=[outpaint_width,outpaint_heighth,outpaint_width_min,outpaint_heighth_min,outpaint_shift_heighth, outpaint_shift_width],show_progress=True, queue=False)    
