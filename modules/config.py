a5de165cda36ee48c75b76764988bb56da5d07eb
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-09-14 16:24:54 +0400

    fix

--- modules/config.py
+++ modules/config.py
@@ -208,6 +208,8 @@ path_wildcards = get_dir_or_set_default('path_wildcards', '../wildcards/')
 path_safety_checker = get_dir_or_set_default('path_safety_checker', '../models/safety_checker/')
 path_sam = get_dir_or_set_default('path_sam', '../models/sam/')
 path_yolo = Path(__file__).parent.parent / "models" / "detection"
+path_video_models = get_dir_or_set_default('path_video_models', '../models/video/')
+path_video_runtime = get_dir_or_set_default('path_video_runtime', '../models/video_runtime/')
 path_outputs = get_path_output()
 
 
