b69d6e896ded83b97cc6a5c737acc21686589850
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-09-21 15:55:24 +0400

    fix

--- webui.py
+++ webui.py
@@ -835,7 +835,7 @@ with shared.gradio_root:
             with gr.Row(elem_classes='extend_row'):
               with gr.Accordion('Extention', open=False):
                 with gr.Accordion('in generation', open=False,elem_classes="nested-accordion") as gen_acc:
-                        with gr.TabItem(label='Florance2')
+                        with gr.TabItem(label='Florance2'):
                             florance2.ui()
                             
 
