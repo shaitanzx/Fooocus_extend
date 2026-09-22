ca5e4315f2127483a602b89cd43e0a22e345c48b
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-09-22 13:04:13 +0400

    fix

--- webui.py
+++ webui.py
@@ -74,6 +74,7 @@ import extentions.cleaner.clean_up_tab as cleaner
 import extentions.omost.main as omost
 from extentions.omost.omost_prompt_builder import process_canvas as omost2prompt
 import extentions.florance2 as florance2
+import extentions.caption as caption
 choices_ar1=["Any", "1:1", "3:2", "4:3", "4:5", "16:9"]
 choices_ar2=["Any", "1:1", "2:3", "3:4", "5:4", "9:16"]
 
@@ -835,6 +836,8 @@ with shared.gradio_root:
             with gr.Row(elem_classes='extend_row'):
               with gr.Accordion('Extention', open=False):
                 with gr.Accordion('in generation', open=False,elem_classes="nested-accordion") as gen_acc:
+                        with gr.TabItem(label='caption'):
+                            caption.gui.gui()
                         with gr.TabItem(label='Florance2'):
                             florance2.ui()
                             
